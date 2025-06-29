7451b6892e95b3cc8157eb1d1e9501aa6142d58e
Nicholas <nickhieb77@gmail.com>
2025-06-29 15:26:37 -0500

    Initial Commit

---
+++ TopTenVer2/TopTenVer2App.swift
@@ -0,0 +1,32 @@
+//
+//  TopTenVer2App.swift
+//  TopTenVer2
+//
+//  Created by Nicholas M Hieb on 6/29/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TopTenVer2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TopTenVer2Tests/TopTenVer2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenVer2Tests.swift
+//  TopTenVer2Tests
+//
+//  Created by Nicholas M Hieb on 6/29/25.
+//
+
+import Testing
+@testable import TopTenVer2
+
+struct TopTenVer2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
